5191de176e79f96b622a0c05d45cd60a9006f500
Kindasus <102754165+thecodedev22@users.noreply.github.com>
2024-09-27 13:56:07 +0100

    app.tsx

---
+++ src /app.tsx
@@ -0,0 +1,14 @@
+import './globals.css';
+
+const App = () => {
+  return (
+          <h1 className="text-3xl font-bold underline">
+            Hello world!
+          </h1>
+        )
+      }</h1>
+    </div>
+  );
+};
+
+export default App;
